6709a25ba071e0af00e8ba1a4cd4d6741e45586a
abashlak <abashlak@waverleysoftware.com>
2017-11-19 01:32:07 +0200

    Add listeners, create simple test with parametr

---
+++ src/main/java/listeners/TestListener.java
@@ -0,0 +1,48 @@
+package listeners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+@Slf4j
+public class TestListener implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult result) {
+        log.info("Now I'm starting testing : %s.%s",
+                result.getTestClass().getName(),result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        log.info("PASS : %s.%s",
+                result.getTestClass().getName(),result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        log.info("FAIL : %s.%s",
+                result.getTestClass().getName(),result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        log.info("SKIP : %s.%s",
+                result.getTestClass().getName(),result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+
+    }
+}
