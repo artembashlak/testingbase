6adeeb50912fe740ccd86c4db40623f291d1ad6e
abashlak <abashlak@waverleysoftware.com>
2017-11-22 12:18:57 +0200

    getenv for github add

--- src/test/java/com/company/GithubLogin.java
+++ src/test/java/com/company/GithubLogin.java
@@ -1,19 +1,24 @@
 package com.company;
 
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 import pages.GitHubLoginPage;
 
 import static core.WebDriverListener.getDriver;
 
+/**
+ * Simple login : github.com
+ */
+
+@Slf4j
 public class GithubLogin {
 
   @Test
   public void loginWIthValidCredentials() throws InterruptedException {
-    getDriver().get("https://github.com/login");
+    getDriver().get(System.getenv("URL"));
+    log.info(System.getenv("URL"));
 
     new GitHubLoginPage()
             .login("artembashlak@gmail.com", "hello123.");
-    Thread.sleep(5000);
-
   }
 }
